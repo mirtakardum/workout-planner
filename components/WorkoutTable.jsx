5227cf365ce4fb92ecb2832d48a01bc4e1509ba1
vlunic00 <vlunic00@fesb.hr>
2024-12-10 14:38:25 +0100

    Started work on the workout table

---
+++ components/WorkoutTable.jsx
@@ -0,0 +1,74 @@
+
+
+function WorkoutTable(){
+    return(
+        <>
+        <div class="relative overflow-x-auto">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Exercise
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Weight
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Sets
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Reps
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="bg-white border-b">
+                        <td class="px-6 py-4 font-medium text-gray-900">
+                            Apple MacBook Pro 17"
+                        </td>
+                        <td class="px-6 py-4">
+                            Silver
+                        </td>
+                        <td class="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td class="px-6 py-4">
+                            $2999
+                        </td>
+                    </tr>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Microsoft Surface Pro
+                        </th>
+                        <td class="px-6 py-4">
+                            White
+                        </td>
+                        <td class="px-6 py-4">
+                            Laptop PC
+                        </td>
+                        <td class="px-6 py-4">
+                            $1999
+                        </td>
+                    </tr>
+                    <tr class="bg-white dark:bg-gray-800">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Magic Mouse 2
+                        </th>
+                        <td class="px-6 py-4">
+                            Black
+                        </td>
+                        <td class="px-6 py-4">
+                            Accessories
+                        </td>
+                        <td class="px-6 py-4">
+                            $99
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        </>
+    )
+}
+
+export default WorkoutTable;
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'wp-dark' : '#1a1818'
       },
     },
   },
