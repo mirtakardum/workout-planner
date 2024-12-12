68461477d8020236b140f93d0e6066c59ab89bb9
mirtakardum <mkardu02@fesb.hr>
2024-12-12 14:21:43 +0100

    Added content to hero card

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="flex-column h-screen w-screen p-12 bg-wp-dark">
       <div className="flex gap-[20px]">
         <HeroCard/>
-        <div className="h-[300px] w-[300px] bg-white" />
+        <div className="bg-white" />
       </div>
     </div>
   );
--- components/HeroCard.js
+++ components/HeroCard.js
@@ -1,11 +1,22 @@
+import Image from "next/image";
 
 
 function HeroCard() {
     return(
         <>
         <div className="grid grid-cols-60/40 w-full gap-2 p-6 bg-white border border-gray-200 rounded-lg">
-            <div className="h-[30px] w-full">
-                <h1 className="font-poppins">Welcome, user.</h1>
+            <div className="w-full">
+                <div>
+                    <h1 className="font-nunito font-extrabold text-[40px]">Welcome back, Mirta!</h1>
+                    <h2 className="font-poppins text-[25px] pt-2 ">Today is upper day, are you ready?</h2>
+                </div>
+                <div>
+                    <h1 className="font-poppins text-[18px] pt-2">Suggested playlist of the day:</h1>
+                    <div className="flex items-end gap-2">
+                        <Image src="/playlistcover1.jpg" width={100} height={100} alt="Playlist cover"/>
+                        <h2 className="font-poppins text-[18px]">90s Rap/90s Hiphop</h2>
+                    </div>
+                </div>
             </div>
             <div className="h-[30px] w-full bg-black" />
         </div>
